7892a6f25b510c22a13055d9ef77412122bfb651
joyap3 <joyapuryear@gmail.com>
2017-06-27 17:24:02 -0400

    Fixed loop and comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,35 +2,75 @@ package com.company;
 
 import java.util.Scanner;
 
+//This program checks the perimeter, area and volume of various classrooms at Grand Circus based on user input.
+/*Prompt the user to enter values for L & W
+Get user input
+Calculate area and perimeter
+Display area and perimeter
+Ask if the user wants to continue
+ */
 
 public class Main {
 
     public static void main(String[] args) {
-        //This program check the perimeter and area of various classrooms at Grand Circus based on user input.
-        Scanner scanner = new Scanner(System.in);
 
+
+        Scanner scanner = new Scanner(System.in);
+//declares variables
         double area;
         double perimeter;
         double length;
         double width;
-        char = userChar = 'n';
+        double height;
+        double volume;
+        char userChar = 'y';
 
-        while (userChar != 'n'){
+//Loops program
+        while (userChar == 'y') {
 
-        System.out.println("Enter Length:");
-        System.out.println("Enter Width:");
+//prompts for user input
+            System.out.println("Enter Length:");
 
-        length = scanner.nextDouble();
-        width = scanner.nextDouble();
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Your input needs to be a in the following format ex: 25.0 ");
+                scanner.nextLine();
+            }
+            length = scanner.nextDouble();
 
+            System.out.println("Enter Width:");
 
-        area =  length * width;
-        perimeter = (2 * length + 2 * width);
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Your input needs to be a in the following format ex: 25.0 ");
+                scanner.nextLine();
+            }
+                width = scanner.nextDouble();
 
-        System.out.println("Area:" + area);
-        System.out.println("Perimeter:" + perimeter);
+//capture user input for height
+                System.out.println("Enter Height:");
 
+                while (!scanner.hasNextDouble()) {
+                    System.out.println("Your input needs to be a in the following format ex: 25.0 ");
+                    scanner.nextLine();
+                }
+                    height = scanner.nextDouble();
 
+//calculates area, perimeter and volume.
+                    area = length * width;
+                    perimeter = 2 * (length + width);
+                    volume = length * height * width;
 
-    }
-}
+//Prints area and perimeter
+                    System.out.println("Area:" + area);
+                    System.out.println("Perimeter:" + perimeter);
+                    System.out.println("Height:" + volume);
+//clears the scanner
+                    scanner.nextLine();
+//prompts if the user wants to find the area of another room
+                    System.out.println("Continue? (y/n)");
+                    userChar = scanner.next().charAt(0);
+                    
+                }
+
+                System.out.println("Goodbye.");
+            }
+        }
